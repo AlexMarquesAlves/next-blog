8227037e71acb11fc6e19999f92e2064882e43b4
¡Duque <atanaelleonardo@gmail.com>
2022-12-30 19:23:10 -0300

    𝑺𝒆𝒕𝒕𝒊𝒏𝒈 𝒖𝒑 𝒆𝒔𝒍𝒊𝒏𝒕 & 𝒑𝒓𝒆𝒕𝒕𝒊𝒆𝒓

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+};
