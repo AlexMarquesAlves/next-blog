254f52be593748afb6e436b4e5ca75eadb5dbbb5
¡Duque <atanaelleonardo@gmail.com>
2022-12-31 22:45:28 -0300

    𝑭𝒐𝒐𝒕𝒆𝒓 𝒄𝒐𝒅𝒆 𝒔𝒕𝒓𝒖𝒄𝒕𝒖𝒓𝒆

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,19 @@ import { Container } from "./styles";
 export const Footer = () => {
   return (
     <Container>
-      <h1>Footer</h1>
+      Feito com ❤ por ¡Duque <br />© 2023 Copyright - Todos os direitos
+      reservados
+      {/* <span>
+        The information provided by ¡Duque, is for general informational
+        purposes only. All information on the Site is provided in good faith,
+        however we make no representation or warranty of any kind, express or
+        implied, regarding the accuracy, adequacy, validity, reliability,
+        availability, or completeness of any information on . UNDER NO
+        CIRCUMSTANCE SHALL WE HAVE ANY LIABILITY TO YOU FOR ANY LOSS OR DAMAGE
+        OF ANY KIND INCURRED AS A RESULT OF THE USE OF OR RELIANCE ON ANY
+        INFORMATION PROVIDED ON . YOUR USE OF AND YOUR RELIANCE ON ANY
+        INFORMATION ON IS SOLELY AT YOUR OWN RISK.
+      </span> */}
     </Container>
   );
 };
