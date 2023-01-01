a682e54b852ed1f747df22ba24bfdaff5ee9f10d
¡Duque <atanaelleonardo@gmail.com>
2023-01-01 14:57:41 -0300

    𝑺𝒕𝒚𝒍𝒆𝒅 𝑯𝒆𝒂𝒅𝒊𝒏𝒈 𝒑𝒐𝒔𝒕 𝒄𝒐𝒎𝒑𝒐𝒏𝒆𝒏𝒕

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from "styled-components";
 
 export const Container = styled.div`
-
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.superLarge};
+    margin: ${theme.spacings.large};
+    text-align: center;
+  `}
 `;
